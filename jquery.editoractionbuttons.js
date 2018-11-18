d674f51af3e1952933ab687d0780a9be761f239c
dardsmind <dardsmind@users.noreply.github.com>
2018-11-18 15:15:01 +0400

    Create jquery.editoractionbuttons.js
    created this jquery file

---
+++ jquery.editoractionbuttons.js
@@ -0,0 +1,53 @@
+/*
+ * jQuery editoractionbuttons: A tiny plugin to put edit, save and cancel button around inline editor area
+ *
+ * Copyright: Dario L. Mindoro
+ *
+ * Contributions and bug reports: https://github.com/claviska/jquery-minicolors
+ *
+ * @license: http://opensource.org/licenses/MIT
+ *
+ */
+(function( $ ) {
+ 
+    $.fn.editorActionButtons = function(options) {
+		
+        var settings = $.extend({
+            onSave: function() {},
+			onEdit: function() {},
+			onCancel: function() {} 			
+        }, options );		
+		
+		var container = $(this);
+		var btnGroup =$("<div class='btn-group pull-right btnEditorActionButtonGroup'><div>");
+		var editButton=$("<button class='btn grey-mint btn-sm btn-outline '><i class='fa fa-pencil'></i></button>");
+		var saveButton=$("<button class='btn grey-mint btn-sm btn-outline '><i class='fa fa-save'></i></button>");
+		var cancelButton=$("<button class='btn grey-mint btn-sm btn-outline '><i class='fa fa-undo'></i></button>");
+		btnGroup.append(editButton);
+		btnGroup.append(saveButton);
+		btnGroup.append(cancelButton);
+		container.append(btnGroup);
+		saveButton.hide();
+		cancelButton.hide();
+		editButton.on( "click", function(){
+			saveButton.show();
+			cancelButton.show();
+			editButton.hide();
+			settings.onEdit.call();
+		});
+		saveButton.on( "click", function(){
+			saveButton.hide();
+			cancelButton.hide();
+			editButton.show();
+			settings.onSave.call();			
+		});
+		cancelButton.on( "click", function(){
+			saveButton.hide();
+			cancelButton.hide();
+			editButton.show();
+			settings.onCancel.call();			
+		});
+        return this;
+    };
+ 
+}( jQuery ));
